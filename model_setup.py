81d8480fb484ce703947a4f0bea1e9c92e11cadd
Adam Karvonen <adam.karvonen@gmail.com>
2024-01-23 14:34:32 -0600

    Add a check for proper amount of layers

--- model_setup.py
+++ model_setup.py
@@ -36,6 +36,8 @@ model_name = "lichess_16layers_ckpt_no_optimizer.pt"
 n_heads = 8
 n_layers = 16  # NOTE: This is pretty janky. You can infer the number of layers from the model name, so use that to set this.
 
+assert str(n_layers) in model_name
+
 if not os.path.exists(f"{MODEL_DIR}{model_name}"):
     state_dict = utils.download_file_from_hf("adamkarvonen/chess_llms", model_name)
     model = torch.load(state_dict, map_location=device)
